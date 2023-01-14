2d277399d8c24bbbea3d39f0b6f1f3c94da6565b
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-13 23:26:45 -0500

    Create Date212List.java

---
+++ Date212List.java
@@ -0,0 +1,58 @@
+public class Date212List {
+   
+   protected  Date212Node first = new Date212Node(null);
+   protected  Date212Node last = first;
+   protected  int length = 0;
+   
+   public int getLength() {
+      return length;
+   }
+   
+   public void append(Date212 d) {
+      Date212Node t = new Date212Node(d);
+      last.next = t;
+      last = t;
+      length++;
+   }
+  
+   public void sortedInsert(Date212 d) {
+	  Date212Node Node = new Date212Node(d);
+	     Date212Node current = first;
+	    while(current.next != null && current.next.data.getYear() < Node.data.getYear()) {
+	    		current = current.next;
+	      }
+	      	Node.next = current.next;
+	      		current.next = Node;
+	   }
+   
+  public String printDate212List(){
+	    String s = "";
+	    Date212Node p = first.next;
+
+	    while(p != null){
+	            s += p.data.toString() + "\n";
+	            p = p.next;
+	        }
+	        return s;
+
+	}
+ 
+   public boolean equals(Object other) {
+    if (other == null || getClass() != other.getClass()|| length != ((Date212List) other).length)
+          return false;
+
+      Date212Node nodeThis = first;
+      Date212Node nodeOther = ((Date212List) other).first;
+      while (nodeThis != null) {
+         if (nodeThis.data != nodeOther.data)
+              return false;
+
+          nodeThis = nodeThis.next;
+          nodeOther = nodeOther.next;
+      } 
+
+      return true;
+  } 
+   
+ 
+}
