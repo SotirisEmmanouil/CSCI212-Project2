67deae84567d4fbc1638f4785e6fb01f495f5fbd
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-14 11:53:40 -0500

    Update Date212Node.java

--- Date212Node.java
+++ Date212Node.java
@@ -1,9 +1,9 @@
 public class Date212Node {
    
- protected Date212 data;
+ protected Date212 data;				//protected variables 
  protected Date212Node next;
 	   
-	   public Date212Node(Date212 d) {
+	   public Date212Node(Date212 d) {		//Date212Node constructor 
 	      data = d;
 	      next = null;
 	   }
