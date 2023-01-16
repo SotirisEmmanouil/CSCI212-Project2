1e20ea4f0e97746483f3001adf44c5328731b361
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-15 20:10:13 -0500

    Update SortedDate212List.java

--- SortedDate212List.java
+++ SortedDate212List.java
@@ -1,6 +1,6 @@
 public class SortedDate212List extends Date212List{
 
-   public void sortedInsert(Date212 d) {		//append a node in a sorted fashion
+   public void add(Date212 d) {		//append a node in a sorted fashion
 	  Date212Node Node = new Date212Node(d);
 	     Date212Node current = first;
 	    while(current.next != null && current.next.data.getYear() < Node.data.getYear()) {
