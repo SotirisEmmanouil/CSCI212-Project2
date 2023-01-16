6806439cae3d8da7d107143bfd0d2b432f61e268
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-15 20:12:59 -0500

    Update UnsortedDate212List.java

--- UnsortedDate212List.java
+++ UnsortedDate212List.java
@@ -1,5 +1,8 @@
 public class UnsortedDate212List extends Date212List{
-	
+    
+    public UnsorteddDate212List() {
+		super();
+	}
 	public void add(Date212 w) {
 		  super.append(w);		//append a node (unsorted)
 	}
